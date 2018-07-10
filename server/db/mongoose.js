b694954957fc8c75c0f90c2ca475f4c05ba2e768
Bharti Bulchandani <bharti@paynpark.xyz>
2018-07-10 11:18:04 +0530

    db changes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-	localhost: 'mongodb://127.0.0.1:27017/Todo',
-	mLab: 'mongodb://bharti25:BhartiIsro2511@ds131551.mlab.com:31551/todoapp'
-};
+// let db = {
+// 	localhost: 'mongodb://127.0.0.1:27017/Todo',
+// 	mLab: 'mongodb://bharti25:BhartiIsro2511@ds131551.mlab.com:31551/todoapp'
+// };
 
-mongoose.connect(db.localhost || db.mLab);
+// mongoose.connect(db.localhost || db.mLab);
+
+mongoose.connect( process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Todo');
 
 module.exports = {
 	mongoose
